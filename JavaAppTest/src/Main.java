1b7739eeec858fb2c6eb11c06851523caa99d878
vusalaxndzde <vusal.axndzde@gmail.com>
2022-09-13 21:19:29 +0400

    main changed4

--- JavaAppTest/src/Main.java
+++ JavaAppTest/src/Main.java
@@ -10,5 +10,7 @@ public class Main {
         System.out.println("salam");
         System.out.println("salam");
         System.out.println("salam");
+        System.out.println("salam");
+        System.out.println("salam");
     }
 }
